3c792f0cd8e83d47a1e2fd8ac4c9715e8aa445b9
Seif Hatem <seifhatem@me.com>
2020-04-07 16:07:16 +0200

    Updating Movie Model to work with API model

--- Feshar/Movies/MovieComponents/Movie.swift
+++ Feshar/Movies/MovieComponents/Movie.swift
@@ -7,23 +7,59 @@
 //
 
 import Foundation
-struct Movie:Equatable{
+//struct Movie:Equatable{
+//    static func == (lhs: Movie, rhs: Movie) -> Bool {
+//        return lhs.title == rhs.title
+//    }
+//
+//    var id: Int
+//    var title: String
+//    var posterIdentifier: String
+//    var genre: genre
+//    var duration: String
+//    var cast: [Cast]
+//    var imdbRating: String
+//    var tags: [Tag]
+//    var description: String
+//    var genreWithDuration: String{
+//        get{
+//            return genre.rawValue + " - " + duration
+//        }
+//    }
+//}
+
+struct Movie:Equatable,Decodable{
     static func == (lhs: Movie, rhs: Movie) -> Bool {
         return lhs.title == rhs.title
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case title = "title"
+        case imdbRating = "vote_average"
+        case description = "overview"
+        case posterIdentifier = "poster_path"
     }
     
     var id: Int
     var title: String
     var posterIdentifier: String
-    var genre: genre
-    var duration: String
-    var cast: [Cast]
-    var imdbRating: String
-    var tags: [Tag]
+    var posterData: Data?
+    var imdbRating: Double
     var description: String
     var genreWithDuration: String{
         get{
-            return genre.rawValue + " - " + duration
+            return ""
         }
     }
 }
+
+extension Decodable {
+  init(from: Any) throws {
+    let data = try JSONSerialization.data(withJSONObject: from, options: .prettyPrinted)
+    let decoder = JSONDecoder()
+    self = try decoder.decode(Self.self, from: data)
+  }
+}
+
+
