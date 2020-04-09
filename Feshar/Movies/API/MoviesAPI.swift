e62c17b69b03c2d240dc4f3257921a267e8d102c
Seif Hatem <seifhatem@me.com>
2020-04-09 07:55:56 +0200

    Create session and watchlist updates

--- Feshar/Movies/API/MoviesAPI.swift
+++ Feshar/Movies/API/MoviesAPI.swift
@@ -7,11 +7,11 @@
 //
 
 import Foundation
-    fileprivate let API_KEY = "837a91247630f256e2090a53ce2c831b"
-    var loginSession: CreateSessionWithLoginResponse?
-    
+fileprivate let API_KEY = "837a91247630f256e2090a53ce2c831b"
+var loginSession: CreateSessionResponse?
+
 class MoviesAPI{
-   
+    
     
     enum Endpoints{
         static let baseURL = "https://api.themoviedb.org/3/"
@@ -22,8 +22,10 @@ class MoviesAPI{
         case FetchMoviesListURL
         case FetchPosterImageURL
         case FetchPersonURL
+        case CreateSessionWithLoginURL
         case CreateSessionURL
-
+        case GetWatchListURL
+        
         
         var stringValue: String {
             switch self {
@@ -31,87 +33,101 @@ class MoviesAPI{
             case .FetchMoviesListURL: return Endpoints.baseURL + "trending/movie/week?page=1&sort_by=release_date.desc"
             case .FetchPosterImageURL:  return Endpoints.postersBaseURL
             case .FetchPersonURL:    return Endpoints.baseURL + "person/"
-            case .CreateSessionURL: return Endpoints.baseURL + "authentication/token/validate_with_login"
+            case .CreateSessionWithLoginURL: return Endpoints.baseURL + "authentication/token/validate_with_login"
+            case .GetWatchListURL: return Endpoints.baseURL + "account/1/watchlist/movies?session_id="
+            case .CreateSessionURL: return Endpoints.baseURL + "authentication/session/new"
+                
             }
         }
         
     }
-        
-//        func createRequestToken() -> CreateRequestTokenResponse?{
-//            var data = httpGETRequest(urlString: Endpoints.CreateRquestTokenURL.stringValue) { (responseData, error) in
-//                if error != nil {
-//                    return nil
-//                }
-//
-//                guard let responseData = responseData else{return nil}
-//                return responseData
-//            }
-//
-//            let decoder = JSONDecoder()
-//            do {
-//                let responseObject = try decoder.decode(CreateRequestTokenResponse.self, from: data)
-//                return responseObject
-//            } catch {
-//               return nil
-//            }
-//
-//
-//        }
+    
+    //        func createRequestToken() -> CreateRequestTokenResponse?{
+    //            var data = httpGETRequest(urlString: Endpoints.CreateRquestTokenURL.stringValue) { (responseData, error) in
+    //                if error != nil {
+    //                    return nil
+    //                }
+    //
+    //                guard let responseData = responseData else{return nil}
+    //                return responseData
+    //            }
+    //
+    //            let decoder = JSONDecoder()
+    //            do {
+    //                let responseObject = try decoder.decode(CreateRequestTokenResponse.self, from: data)
+    //                return responseObject
+    //            } catch {
+    //               return nil
+    //            }
+    //
+    //
+    //        }
 }
-        func httpGETRequest(urlString: String, completion: @escaping ( _ responseData: Data?, _ error: Error?) -> Void){
-               var urlStringWithKey = ""
-            
-                if (urlString.contains("?")){
-                    urlStringWithKey = urlString + "&api_key=" + API_KEY
-                }
-                else
-                {
-                    urlStringWithKey = urlString + "?api_key=" + API_KEY
-                }
-            
-            let url = URL(string: urlStringWithKey)
-            
-            
-            let configuration = URLSessionConfiguration.default
-            configuration.allowsCellularAccess = false
-            
-            let backgroundSession = URLSession(configuration: configuration)
-            
-            
-            let task = backgroundSession.dataTask(with: url!) {data, httpresponse, error in
-                
-                //print("HTTP request completed")
-                if let error = error{
-                    print("HTTP Request Error: " + error.localizedDescription)
-                }
-                
-                if let _ = data{
-                   
-                    
-                } else{
-                    print("Returned data is nil")
-                    return
-                }
-                
-                completion(data,error)
-                
-            }
-            
-            task.resume()
-            //print("HTTP Request started")
-            
+func httpGETRequest( urlString: String, completion: @escaping ( _ responseData: Data?, _ error: Error?) -> Void){
+    var urlStringWithKey = ""
+    
+    if let session = loginSession?.session_id, urlString.contains("session_id") {
+        urlStringWithKey = urlString + session
+    }
+    else
+    {
+        urlStringWithKey = urlString
+    }
+    
+    
+    if (urlStringWithKey.contains("?")){
+        urlStringWithKey = urlStringWithKey + "&api_key=" + API_KEY
+    }
+    else
+    {
+        urlStringWithKey = urlStringWithKey + "?api_key=" + API_KEY
+    }
+    
+    print(urlStringWithKey)
+    
+    let url = URL(string: urlStringWithKey)
+    
+    
+    let configuration = URLSessionConfiguration.default
+    configuration.allowsCellularAccess = false
+    
+    let backgroundSession = URLSession(configuration: configuration)
+    
+    
+    let task = backgroundSession.dataTask(with: url!) {data, httpresponse, error in
+        
+        //print("HTTP request completed")
+        if let error = error{
+            print("HTTP Request Error: " + error.localizedDescription)
         }
+        
+        if let _ = data{
+            
+            
+        } else{
+            print("Returned data is nil")
+            return
+        }
+        
+        completion(data,error)
+        
+    }
+    
+    task.resume()
+    //print("HTTP Request started")
+    
+}
 
 func httpPOSTRequest(urlString: String, postData: Data, completion: @escaping ( _ responseData: Data?, _ error: Error?) -> Void){
-       var urlStringWithKey = ""
+    var urlStringWithKey = ""
     
-        if (urlString.contains("?")){
-            urlStringWithKey = urlString + "&api_key=" + API_KEY
-        }
-        else
-        {
-            urlStringWithKey = urlString + "?api_key=" + API_KEY
-        }
+    if (urlString.contains("?")){
+        urlStringWithKey = urlString + "&api_key=" + API_KEY
+    }
+    else
+    {
+        urlStringWithKey = urlString + "?api_key=" + API_KEY
+    }
     
     let url = URL(string: urlStringWithKey)
     
@@ -133,7 +149,7 @@ func httpPOSTRequest(urlString: String, postData: Data, completion: @escaping ( 
         }
         
         if let _ = data{
-           
+            
             
         } else{
             print("Returned data is nil")
