6674c5aadca037e7fa892e7a364414d29f727ed7
Seif Hatem <seifhatem@me.com>
2020-04-10 16:05:18 +0200

    Logout UI

---
+++ Feshar/Movies/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileViewController.swift
+//  Feshar
+//
+//  Created by Seif Elmenabawy on 4/10/20.
+//  Copyright © 2020 Seif Elmenabawy. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
